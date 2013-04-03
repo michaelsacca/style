879e9051cbeae921fe5956fd01a4e1ff0b759c05
msacca <msacca@tinyfactory.co>
2013-04-02 18:53:06 -0700

    moved JS directory

--- js/redirect.js
+++
@@ -1,6 +0,0 @@
-/*This handles when the user is redirected to our fake URL*/
-// send a request to our background.js to set the access token
-chrome.extension.sendRequest({type:'auth', token:window.location.hash}, function (response) {
-    window.open('', '_self', '');
-    window.close();
-});
